40c6207b5d8124eb2a058a750d8a15dd202928f9
GleenMike <rahmatrafid22@gmail.com>
2023-01-29 05:35:38 +0700

    Add Form input and output

--- cipher/src/App.js
+++ cipher/src/App.js
@@ -1,13 +1,13 @@
 import { useState } from 'react';
 import './App.css';
-import Navbar from './components/Navbar'
-
+import Navbar from './components/Navbar';
+import {InputString, InputFormula, InputMatrix} from './components/InputKey';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <GetInput />
+      <GetInput type="matrix"/>
       <ShowResult />
     </div>
   );
@@ -15,33 +15,38 @@ function App() {
 
 function GetInput(props) {
   const [text, setText] = useState('');
-  const [key, setKey] = useState('');
-
   function handleTextChange(event) {
     setText(event.target.value);
   }
-
-  function handleKeyChange(event) {
-    setKey(event.target.value);
-  }
-
+  
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Input 1: ", text);
-    console.log("Input 2: ", key);
+    alert(`Masukkan Text: ${text}`);
+    // alert(`Input 2: ${key}`);
+  }
+
+  let inputKey;
+  if (props.type === "string") {
+    inputKey = <InputString />
+  }
+  else if (props.type === "formula") {
+    inputKey = <InputFormula />
+  }
+  else if (props.type === "matrix"){
+    inputKey = <InputMatrix />
   }
 
   return (
     <div className="InputForm">    
       <form onSubmit={handleSubmit}>
-        <label>Enter Your text:</label>
-        <input type="text" value={text} onChange={handleTextChange} />
-        <p>Input value: {text}</p>
-        <br />
-        <label>Enter Your Key:</label>
-        <input type="text" value={key} onChange={handleKeyChange} />
-        <p>Input value: {key}</p>
-        <br />
+        <div className='IOtext'>
+          <label>
+            Enter Your Text:
+          </label>
+          <textarea type="text" value={text} onChange={handleTextChange} />
+          <p>Input value: {text}</p>
+        </div>
+        {inputKey}
         <button type="submit">Submit</button>
       </form>
     </div>
@@ -49,15 +54,19 @@ function GetInput(props) {
 }
 
 function ShowResult(props) {
-  const [result, setResult] = useState('');
+  let [result, setResult] = useState('');
+  result = "Dari komputasi";
   function handleResultChange(event) {
     setResult(event.target.value);
   }
   
   return(
     <div className="OutputForm">
-      <label>Result:</label>
-      <input values = {result}></input>
+      <div className='IOtext'>
+        <label>Result:
+        </label>
+        <textarea value={result}></textarea>
+      </div>
     </div>
   );
 }
